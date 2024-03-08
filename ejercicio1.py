e9318cca85cf7f5a3ac06a01d98562bc25a029c5
Andres Veintimilla <andveti@gmail>
2024-03-08 09:14:59 -0500

    Ejercicio realizado con 4 aciertos en pytest

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,8 +1,14 @@
 class Coche:
     def __init__(self, marca, modelo):
-        # Asigna la marca proporcionada al atributo 'marca'
-        # Asigna el modelo proporcionado al atributo 'modelo'
+        
+        self.marca = marca  # Asigna la marca proporcionada al atributo 'marca'
+        self.modelo = modelo  # Asigna el modelo proporcionado al atributo 'modelo'
+       
+       
 
     def conducir(self):
         return f"Conduciendo el {self.marca} {self.modelo}"
 
+
+sedan = Coche("Toyota", "Corolla")
+print(sedan.conducir()) 
--- ejercicio2.py
+++ ejercicio2.py
@@ -1,6 +1,6 @@
 class Animal:
     def __init__(self, nombre):
-        # Asigna el nombre proporcionado al atributo 'nombre'
+        self.nombre = nombre  # Asigna el nombre proporcionado al atributo 'nombre'
 
     def hacer_sonido(self):
         pass
@@ -8,7 +8,16 @@ class Animal:
 class Perro(Animal):
     def hacer_sonido(self):
         # Devuelve el sonido de un perro (ladrido)
+        return f"¡Guau!"
 
 class Gato(Animal):
     def hacer_sonido(self):
         # Devuelve el sonido de un gato (maullido)
+        return f"¡Miau!"
+        
+
+perro = Perro("Firulais")
+gato = Gato("Minks")
+
+print (perro.hacer_sonido())  
+print (gato.hacer_sonido())  
